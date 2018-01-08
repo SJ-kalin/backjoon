4f615a234e6a0c17ad667191e150278063cdf198
sj-kalin <hahava5696@gmail.com>
2018-01-08 13:51:14 +0900

    #10871 문제

---
+++ back/src/back/step_4/If/P10871.java
@@ -0,0 +1,29 @@
+package back.step_4.If;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P10871 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int x = scan.nextInt();
+		int a[] = new int[N];
+		ArrayList<Integer> arrayList = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			a[i] = scan.nextInt();
+		}
+
+		for (int i = 0; i < N; i++)
+			if (a[i] < x) {
+				arrayList.add(a[i]);
+			}
+
+		for (int i = 0; i < arrayList.size(); i++) {
+			System.out.print(arrayList.get(i) + " ");
+		}
+	}
+
+}
