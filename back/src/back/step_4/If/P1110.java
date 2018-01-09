e58b7004f3143f6e8f17f6c456093e54516efd10
sj-kalin <hahava5696@gmail.com>
2018-01-09 14:47:42 +0900

    #P1110

---
+++ back/src/back/step_4/If/P1110.java
@@ -0,0 +1,19 @@
+package back.step_4.If;
+
+import java.util.Scanner;
+
+public class P1110 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		int clone = num;
+		int count = 0;
+		do {
+			int first = clone / 10;
+			int second = clone % 10;
+			int sum = (first + second) % 10;
+			clone = second * 10 + sum;
+		} while (num != clone);
+		System.out.println(count);
+	}
+}
