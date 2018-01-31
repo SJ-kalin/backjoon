2d5a35aeaf4d034edb5b08766c8fa186b3e36807
hahav <hahav@DESKTOP-LISFCRV>
2018-01-31 14:37:48 +0900

    #P10809, #P11654

---
+++ back/src/back/step_7/str/P11654.java
@@ -0,0 +1,11 @@
+package back.step_7.str;
+
+import java.util.Scanner;
+
+public class P11654 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char a = scanner.nextLine().charAt(0);
+		System.out.printf("%d", (int) a);
+	}
+}
