e5c6586ad4aa7fccbac15639f7df6a24878ffe8f
sj-kalin <hahava5696@gmail.com>
2018-10-10 14:26:11 +0900

    #3053

---
+++ back/src/back/step_17/imple/P3053.java
@@ -0,0 +1,18 @@
+package back.step_17.imple;
+
+import java.util.Scanner;
+
+public class P3053 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double r = scanner.nextDouble();
+
+        System.out.printf("%.6f\n", r * r * Math.PI);
+        System.out.printf("%.6f\n", r * r * 2);
+
+
+    }
+
+}
